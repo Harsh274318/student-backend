1e0d17688a64597abec10c1f7a7185cb029a6794
Harsh Vardhan Pal <hp77335@gmail.com>
2026-04-06 21:52:46 +0530

    get teacher file edit

--- controllers/getuser/getTeacher.js
+++ controllers/getuser/getTeacher.js
@@ -1,11 +1,13 @@
 import User from "../../models/newUsers/userSchema.js";
 import Teacher from "../../models/newUsers/teacherSchema.js";
 import customRes from "../../utils/customRes.js";
-import Student from "../../models/newUsers/studentSchema.js";
 export async function getTeacher(req, res) {
   try {
+    const userId = req.user.userId
+    if(!userId) return customRes(res,400,false,"","user not found","");
+    const isPrincipal= await User.findOne({_id:userId,role:"Principal"});
+    if(!isPrincipal) return customRes(res,404,false,"","Principal not found","");
     const teachers = await Teacher.find();
-    // .populate("userId", "email");
     if (teachers.length === 0) {
       return customRes(res, 404, false, "", "User not found", "");
     }
@@ -21,3 +23,4 @@ export async function getTeacher(req, res) {
     return customRes(res, 500, false, "", err.message, "");
   }
 }
+export default getTeacher;
--- routes/principalRoutes.js
+++ routes/principalRoutes.js
@@ -3,9 +3,11 @@ import { authUser } from "../middleware/authUser.js";
 import { roleCheck } from "../middleware/roleCheck.js";
 import addSession from "../controllers/principalControllers/addSession.js";
 import studentById from "../controllers/principalControllers/getStudentById.js";
+import getTeacher from "../controllers/getuser/getTeacher.js";
 const principalRoute = express.Router();
 
 
 principalRoute.patch("/session", authUser, roleCheck("Principal"), addSession);
-principalRoute.get("/student/:id", authUser, roleCheck("Principal"), studentById)
-export default principalRoute;
+principalRoute.get("/teachers/info", authUser, roleCheck("Principal"), getTeacher)
+principalRoute.get("/student/:id", authUser, roleCheck("Principal"), studentById);
+export default principalRoute;  
