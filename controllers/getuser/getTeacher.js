25fa49c158298cffd3f4b7ea20b2280888366df2
Harsh Vardhan Pal <hp77335@gmail.com>
2026-04-10 17:06:42 +0530

    geting teacher's details

--- controllers/getuser/getTeacher.js
+++ controllers/getuser/getTeacher.js
@@ -4,10 +4,10 @@ import customRes from "../../utils/customRes.js";
 export async function getTeacher(req, res) {
   try {
     const userId = req.user.userId
-    if(!userId) return customRes(res,400,false,"","user not found","");
-    const isPrincipal= await User.findOne({_id:userId,role:"Principal"});
-    if(!isPrincipal) return customRes(res,404,false,"","Principal not found","");
-    const teachers = await Teacher.find();
+    if (!userId) return customRes(res, 400, false, "", "user not found", "");
+    const isPrincipal = await User.findOne({ _id: userId, role: "Principal" });
+    if (!isPrincipal) return customRes(res, 404, false, "", "Principal not found", "");
+    const teachers = await Teacher.find().populate("userId", "name email url public_id");
     if (teachers.length === 0) {
       return customRes(res, 404, false, "", "User not found", "");
     }
