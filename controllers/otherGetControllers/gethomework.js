f383b2c84e4ee3602cdea2a6cf7bcb180c1283e5
Harsh Vardhan Pal <hp77335@gmail.com>
2026-04-19 14:10:22 +0530

    home work

--- controllers/otherGetControllers/gethomework.js
+++ controllers/otherGetControllers/gethomework.js
@@ -9,20 +9,22 @@ const getHomework = async (req, res) => {
     try {
         const date = req.query.date;
         let classToUse = req.query.useClass
-
         if (!date) return customRes(res, 400, false, "", "Something is wrong", "");
         if (typeof date !== "string") return customRes(res, 400, false, "", "Invalid date format", "");
         const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
         if (!dateRegex.test(date)) return customRes(res, 400, false, "", "Invalid date format (YYYY-MM-DD)", "");
         const session = await Session.findOne();
         const currentSession = session.currentSession;
-        if (!classToUse) {
-            const studentId = req.user.id
+        let studentId = ""
+        let student = null
+        if (!JSON.parse(classToUse) || !classToUse) {
+            studentId = req.user.id
             if (!studentId) return customRes(res, 400, false, "", "Something is wrong", "");
-            const student = await Student.findOne({ userId: studentId, session: currentSession });
+            student = await Student.findOne({ userId: studentId, session: currentSession });
             if (!student) return customRes(res, 404, false, "", "Student not found", "");
             classToUse = student.class
         }
+
         if (isNaN(Number(classToUse))) return customRes(res, 400, false, "", "Class must be a number", "");
         classToUse = Number(classToUse);
         const ishomeWork = await HomeWork.findOne({ class: classToUse, session: currentSession, date })
